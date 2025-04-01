2301ec46621135b194fdbf3d53acd2ee7df4d840
antiacat <antia.cheng@gmail.com>
2025-04-01 16:17:06 +0800

    Add files via upload

---
+++ uuu/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace uuu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            畫筆色彩ToolStripMenuItem = new ToolStripMenuItem();
+            藍ToolStripMenuItem = new ToolStripMenuItem();
+            綠ToolStripMenuItem = new ToolStripMenuItem();
+            黃ToolStripMenuItem = new ToolStripMenuItem();
+            紫ToolStripMenuItem = new ToolStripMenuItem();
+            粉ToolStripMenuItem = new ToolStripMenuItem();
+            黑ToolStripMenuItem = new ToolStripMenuItem();
+            線條粗細ToolStripMenuItem = new ToolStripMenuItem();
+            細ToolStripMenuItem = new ToolStripMenuItem();
+            中ToolStripMenuItem = new ToolStripMenuItem();
+            粗ToolStripMenuItem = new ToolStripMenuItem();
+            線條樣式ToolStripMenuItem = new ToolStripMenuItem();
+            直線ToolStripMenuItem = new ToolStripMenuItem();
+            點短虛線ToolStripMenuItem = new ToolStripMenuItem();
+            長短虛線ToolStripMenuItem = new ToolStripMenuItem();
+            長虛線ToolStripMenuItem = new ToolStripMenuItem();
+            储存檔案ToolStripMenuItem = new ToolStripMenuItem();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            橡皮擦ToolStripMenuItem = new ToolStripMenuItem();
+            panel1 = new Panel();
+            button2 = new Button();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { 畫筆色彩ToolStripMenuItem, 線條粗細ToolStripMenuItem, 線條樣式ToolStripMenuItem, 储存檔案ToolStripMenuItem, 橡皮擦ToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // 畫筆色彩ToolStripMenuItem
+            // 
+            畫筆色彩ToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { 藍ToolStripMenuItem, 綠ToolStripMenuItem, 黃ToolStripMenuItem, 紫ToolStripMenuItem, 粉ToolStripMenuItem, 黑ToolStripMenuItem });
+            畫筆色彩ToolStripMenuItem.Name = "畫筆色彩ToolStripMenuItem";
+            畫筆色彩ToolStripMenuItem.Size = new Size(67, 20);
+            畫筆色彩ToolStripMenuItem.Text = "畫筆色彩";
+            // 
+            // 藍ToolStripMenuItem
+            // 
+            藍ToolStripMenuItem.Name = "藍ToolStripMenuItem";
+            藍ToolStripMenuItem.Size = new Size(86, 22);
+            藍ToolStripMenuItem.Text = "藍";
+            藍ToolStripMenuItem.Click += 藍ToolStripMenuItem_Click;
+            // 
+            // 綠ToolStripMenuItem
+            // 
+            綠ToolStripMenuItem.Name = "綠ToolStripMenuItem";
+            綠ToolStripMenuItem.Size = new Size(86, 22);
+            綠ToolStripMenuItem.Text = "綠";
+            綠ToolStripMenuItem.Click += 綠ToolStripMenuItem_Click;
+            // 
+            // 黃ToolStripMenuItem
+            // 
+            黃ToolStripMenuItem.Name = "黃ToolStripMenuItem";
+            黃ToolStripMenuItem.Size = new Size(86, 22);
+            黃ToolStripMenuItem.Text = "黃";
+            黃ToolStripMenuItem.Click += 黃ToolStripMenuItem_Click;
+            // 
+            // 紫ToolStripMenuItem
+            // 
+            紫ToolStripMenuItem.Name = "紫ToolStripMenuItem";
+            紫ToolStripMenuItem.Size = new Size(86, 22);
+            紫ToolStripMenuItem.Text = "紫";
+            紫ToolStripMenuItem.Click += 紫ToolStripMenuItem_Click;
+            // 
+            // 粉ToolStripMenuItem
+            // 
+            粉ToolStripMenuItem.Name = "粉ToolStripMenuItem";
+            粉ToolStripMenuItem.Size = new Size(86, 22);
+            粉ToolStripMenuItem.Text = "粉";
+            粉ToolStripMenuItem.Click += 粉ToolStripMenuItem_Click;
+            // 
+            // 黑ToolStripMenuItem
+            // 
+            黑ToolStripMenuItem.Name = "黑ToolStripMenuItem";
+            黑ToolStripMenuItem.Size = new Size(86, 22);
+            黑ToolStripMenuItem.Text = "黑";
+            黑ToolStripMenuItem.Click += 黑ToolStripMenuItem_Click;
+            // 
+            // 線條粗細ToolStripMenuItem
+            // 
+            線條粗細ToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { 細ToolStripMenuItem, 中ToolStripMenuItem, 粗ToolStripMenuItem });
+            線條粗細ToolStripMenuItem.Name = "線條粗細ToolStripMenuItem";
+            線條粗細ToolStripMenuItem.Size = new Size(67, 20);
+            線條粗細ToolStripMenuItem.Text = "線條粗細";
+            // 
+            // 細ToolStripMenuItem
+            // 
+            細ToolStripMenuItem.Name = "細ToolStripMenuItem";
+            細ToolStripMenuItem.Size = new Size(86, 22);
+            細ToolStripMenuItem.Text = "細";
+            細ToolStripMenuItem.Click += 細ToolStripMenuItem_Click;
+            // 
+            // 中ToolStripMenuItem
+            // 
+            中ToolStripMenuItem.Name = "中ToolStripMenuItem";
+            中ToolStripMenuItem.Size = new Size(86, 22);
+            中ToolStripMenuItem.Text = "中";
+            中ToolStripMenuItem.Click += 中ToolStripMenuItem_Click;
+            // 
+            // 粗ToolStripMenuItem
+            // 
+            粗ToolStripMenuItem.Name = "粗ToolStripMenuItem";
+            粗ToolStripMenuItem.Size = new Size(86, 22);
+            粗ToolStripMenuItem.Text = "粗";
+            粗ToolStripMenuItem.Click += 粗ToolStripMenuItem_Click;
+            // 
+            // 線條樣式ToolStripMenuItem
+            // 
+            線條樣式ToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { 直線ToolStripMenuItem, 點短虛線ToolStripMenuItem, 長短虛線ToolStripMenuItem, 長虛線ToolStripMenuItem });
+            線條樣式ToolStripMenuItem.Name = "線條樣式ToolStripMenuItem";
+            線條樣式ToolStripMenuItem.Size = new Size(67, 20);
+            線條樣式ToolStripMenuItem.Text = "線條樣式";
+            線條樣式ToolStripMenuItem.Click += 線條樣式ToolStripMenuItem_Click;
+            // 
+            // 直線ToolStripMenuItem
+            // 
+            直線ToolStripMenuItem.Name = "直線ToolStripMenuItem";
+            直線ToolStripMenuItem.Size = new Size(125, 22);
+            直線ToolStripMenuItem.Text = "直線";
+            直線ToolStripMenuItem.Click += 直線ToolStripMenuItem_Click;
+            // 
+            // 點短虛線ToolStripMenuItem
+            // 
+            點短虛線ToolStripMenuItem.Name = "點短虛線ToolStripMenuItem";
+            點短虛線ToolStripMenuItem.Size = new Size(125, 22);
+            點短虛線ToolStripMenuItem.Text = "點 短虛線";
+            點短虛線ToolStripMenuItem.Click += 點短虛線ToolStripMenuItem_Click;
+            // 
+            // 長短虛線ToolStripMenuItem
+            // 
+            長短虛線ToolStripMenuItem.Name = "長短虛線ToolStripMenuItem";
+            長短虛線ToolStripMenuItem.Size = new Size(125, 22);
+            長短虛線ToolStripMenuItem.Text = "長短虛線";
+            長短虛線ToolStripMenuItem.Click += 長短虛線ToolStripMenuItem_Click;
+            // 
+            // 長虛線ToolStripMenuItem
+            // 
+            長虛線ToolStripMenuItem.Name = "長虛線ToolStripMenuItem";
+            長虛線ToolStripMenuItem.Size = new Size(125, 22);
+            長虛線ToolStripMenuItem.Text = "長虛線";
+            長虛線ToolStripMenuItem.Click += 長虛線ToolStripMenuItem_Click;
+            // 
+            // 储存檔案ToolStripMenuItem
+            // 
+            储存檔案ToolStripMenuItem.Name = "储存檔案ToolStripMenuItem";
+            储存檔案ToolStripMenuItem.Size = new Size(67, 20);
+            储存檔案ToolStripMenuItem.Text = "储存檔案";
+            储存檔案ToolStripMenuItem.Click += 储存檔案ToolStripMenuItem_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.Window;
+            pictureBox1.Location = new Point(12, 27);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(586, 411);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(604, 149);
+            button1.Name = "button1";
+            button1.Size = new Size(177, 102);
+            button1.TabIndex = 2;
+            button1.Text = "清除";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // 橡皮擦ToolStripMenuItem
+            // 
+            橡皮擦ToolStripMenuItem.Image = Properties.Resources.images;
+            橡皮擦ToolStripMenuItem.Name = "橡皮擦ToolStripMenuItem";
+            橡皮擦ToolStripMenuItem.Size = new Size(71, 20);
+            橡皮擦ToolStripMenuItem.Text = "橡皮擦";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button2);
+            panel1.Location = new Point(604, 27);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(177, 116);
+            panel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Window;
+            button2.BackgroundImage = Properties.Resources.images1;
+            button2.BackgroundImageLayout = ImageLayout.Zoom;
+            button2.Location = new Point(3, 6);
+            button2.Name = "button2";
+            button2.Size = new Size(171, 103);
+            button2.TabIndex = 0;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "小畫家";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem 畫筆色彩ToolStripMenuItem;
+        private ToolStripMenuItem 線條粗細ToolStripMenuItem;
+        private ToolStripMenuItem 線條樣式ToolStripMenuItem;
+        private ToolStripMenuItem 储存檔案ToolStripMenuItem;
+        private ToolStripMenuItem 細ToolStripMenuItem;
+        private ToolStripMenuItem 中ToolStripMenuItem;
+        private ToolStripMenuItem 粗ToolStripMenuItem;
+        private ToolStripMenuItem 直線ToolStripMenuItem;
+        private ToolStripMenuItem 藍ToolStripMenuItem;
+        private ToolStripMenuItem 綠ToolStripMenuItem;
+        private ToolStripMenuItem 黃ToolStripMenuItem;
+        private ToolStripMenuItem 點短虛線ToolStripMenuItem;
+        private ToolStripMenuItem 長短虛線ToolStripMenuItem;
+        private ToolStripMenuItem 長虛線ToolStripMenuItem;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private ToolStripMenuItem 紫ToolStripMenuItem;
+        private ToolStripMenuItem 粉ToolStripMenuItem;
+        private ToolStripMenuItem 黑ToolStripMenuItem;
+        private ToolStripMenuItem 橡皮擦ToolStripMenuItem;
+        private Panel panel1;
+        private Button button2;
+    }
+}
---
+++ uuu/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System.Drawing.Imaging;
+namespace uuu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 線條樣式ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(800, 600);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+        }
+
+        
+        Pen p = new Pen(Color.Black, 3);
+
+        int x, y; //計畫滑鼠位置
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Graphics g = Graphics.FromImage(pictureBox1.Image);
+                g.DrawLine(p, x, y, e.X, e.Y);
+                x = e.X;
+                y = e.Y;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void 藍ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Blue;
+        }
+
+        private void 綠ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Green;
+        }
+
+        private void 黃ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Yellow;
+        }
+
+        private void 紫ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.MediumPurple;
+        }
+
+        private void 粉ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.LightPink;
+        }
+
+        private void 黑ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Black;
+        }
+
+        private void 細ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Width = 1;
+        }
+
+        private void 中ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Width = 3;
+        }
+
+        private void 粗ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Width = 5;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(800, 600);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+        }
+
+        private void 點短虛線ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+        }
+
+        private void 長短虛線ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+        }
+
+        private void 長虛線ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            p.DashPattern = new float[] { 6.0F, 3.0F };
+        }
+
+        private void 直線ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+        }
+
+        private void 储存檔案ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image.Save(@"C:\Users\User\Desktop\1.jpg", ImageFormat.Png);
+                MessageBox.Show("存檔成功");
+            }
+            catch
+            {
+                MessageBox.Show("存檔失敗");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
